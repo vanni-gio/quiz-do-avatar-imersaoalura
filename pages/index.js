127539d08511a6dda0f39f62cf116d3b4fd63002
vanni-gio <giovannip.gomesss@gmail.com>
2021-01-25 20:53:54 -0300

    código ao final da primeira aula da imersão.

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+import db from '../db.json'
+import QuizBackground from '../src/componentes/QuizBackground'
+import QuizLogo from '../src/componentes/QuizLogo'
+import GitHubCorner from '../src/componentes/GithubCorner'
+import Footer from '../src/componentes/Footer'
+import Widget from '../src/componentes/Widget'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
+
+export default function Home() {
+  return (
+  <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <QuizLogo/>
+      <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+      <Footer/>
+    </QuizContainer>
+    <GitHubCorner projectUrl="https://github.com/vanni-gio" />
+  </QuizBackground>
+  );
+}
